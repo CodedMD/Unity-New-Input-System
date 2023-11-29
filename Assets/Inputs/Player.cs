88abd4565d09a0b2dcda7f2697a29e92cc58aa36
CodedMD <codingcoremd@gmail.com>
2023-11-29 14:41:21 -0800

    Action map Swapping

--- Assets/Inputs/Player.cs
+++ Assets/Inputs/Player.cs
@@ -6,28 +6,52 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     private PlayerInputActions _input;
+    private MeshRenderer _render;
     // Start is called before the first frame update
     void Start()
     {
         _input = new PlayerInputActions();
         _input.Player.Enable();
 
-       
-        
+        _input.Player.Color.performed += Color_performed;
+        _input.Player.DrivingMap.performed += DrivingMap_performed;
+
+        _render = GetComponent<MeshRenderer>();
     }
 
     
-
     // Update is called once per frame
     void Update()
     {
-        Movement();
+        Rotating();
+        Driving();
     }
 
-
-    private void Movement()
+    private void DrivingMap_performed(InputAction.CallbackContext context)
     {
-        var move = _input.Player.Movement.ReadValue<Vector2>();
-        transform.Translate(move * Time.deltaTime * 3);
+        _input.Player.Disable();
+        _input.Driving.Enable();
     }
+
+    private void Driving()
+    {
+        var move = _input.Driving.Drive.ReadValue<Vector2>();
+        transform.Translate(new Vector3(move.x, 0, move.y) * Time.deltaTime * 3f);
+
+    }
+
+
+    private void Rotating()
+    {
+        Debug.Log("Acis Value:" + _input.Player.Rotation.ReadValue<float>());
+        var rotationDirection = _input.Player.Rotation.ReadValue<float>();
+        transform.Rotate(Vector3.up * Time.deltaTime * 30f * rotationDirection);
+    }
+    private void Color_performed(InputAction.CallbackContext context)
+    {
+        _render.material.color = Random.ColorHSV();
+
+    }
+
+
 }
