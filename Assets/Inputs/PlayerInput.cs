69a6388c917a7a5bbc43b37ef44f0e55ea23fd13
CodedMD <codingcoremd@gmail.com>
2023-11-28 16:38:11 -0800

    adding each dog action via script

--- Assets/Inputs/PlayerInput.cs
+++ Assets/Inputs/PlayerInput.cs
@@ -13,8 +13,14 @@ public class PlayerInput : MonoBehaviour
         _input.Dog.Enable();
         _input.Dog.Bark.performed += Bark_performed;
         _input.Dog.Bark.canceled += Bark_canceled;
-       //// _input.Dog.Bark.performed += Bark_performed => Debug.Log("Barking" + Bark_performed);
-        _input.Dog.Bark.canceled += Bark_canceled => Debug.Log("Done Barking" + Bark_canceled);
+        //// _input.Dog.Bark.performed += Bark_performed => Debug.Log("Barking" + Bark_performed);
+        //_input.Dog.Bark.canceled += Bark_canceled => Debug.Log("Done Barking" + Bark_canceled);
+        _input.Dog.Walk.performed += Walk_performed => Debug.Log("Walking" + Walk_performed);
+        _input.Dog.Walk.canceled += Walk_canceled => Debug.Log("Not Walking" + Walk_canceled);
+        _input.Dog.Run.performed += Run_performed => Debug.Log("Running" + Run_performed);
+        _input.Dog.Run.canceled += Run_canceled => Debug.Log("Done Running" + Run_canceled);
+        _input.Dog.Die.performed += Die_performed => Debug.Log("Dying" + Die_performed);
+        _input.Dog.Die.performed += MyCustomAction => Debug.Log("Not Dying" + MyCustomAction);
     }
     
     private void Bark_performed(InputAction.CallbackContext context)
